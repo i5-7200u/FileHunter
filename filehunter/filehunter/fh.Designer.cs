5757b4aa9d2556f7c7e4fb434d90f184855b5d9c
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-08-31 19:47:39 +0300

    Add files via upload

---
+++ filehunter/filehunter/fh.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace filehunter
+{
+    partial class fh
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(562, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "dosyaya yazilacak random yazinin uzunlugunu ayarla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "parçala / shred";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "file lenght";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Shredd Integer = 0";
+            // 
+            // fh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 137);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(602, 176);
+            this.MinimumSize = new System.Drawing.Size(602, 176);
+            this.Name = "fh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileHunter V1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ filehunter/filehunter/fh.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filehunter
+{
+    public partial class fh : Form
+    {
+        long filelenghtcontrol;
+        int randomchararraylenght = 3072;
+        int shreddedcontrol = 0;
+        public fh()
+        {
+            InitializeComponent();
+        }
+    //downstairs code invalid
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) > 50)
+            {
+                MessageBox.Show("Maksimum 50 MB Ayarlayabilirsiniz uzunluğu!", "FH", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (int.Parse(textBox1.Text) < 7)
+            {
+                MessageBox.Show("Minimum 7 MB Ayarlayabilirsiniz uzunluğu!", "FH", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                randomchararraylenght = int.Parse(textBox1.Text) * 1024;
+                MessageBox.Show("Ayarlandi!", "FH", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            MessageBox.Show(Convert.ToString(randomchararraylenght));
+        }
+        //downstairs code valid
+        public string randomcode()
+        {
+            Random randomcreate = new Random();
+            int randominteger;
+            string randomchararray = "";
+            for (int whilestopper = 1; whilestopper > 0;)
+            {
+                randominteger = randomcreate.Next(50, 100);
+                randomchararray += Convert.ToChar(randominteger);
+                if (randomchararray.Length == randomchararraylenght)
+                    {
+                    whilestopper = 0;
+                }
+            }
+            return randomchararray;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                timer1.Start();
+                }
+        }
+
+        public void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.IO.FileInfo fileinformation = new System.IO.FileInfo(openFileDialog1.FileName);
+            long filelenght = fileinformation.Length;
+            System.IO.File.WriteAllText(openFileDialog1.FileName, "");
+            for (int whilestopper = 1; whilestopper > 0;)
+            {
+                if (filelenghtcontrol > filelenght)
+                {
+                    shreddedcontrol++;
+                    System.IO.File.WriteAllText(openFileDialog1.FileName, "");
+                }
+                if (shreddedcontrol == 60)
+                {
+                    whilestopper = 0;
+                    shreddedcontrol = 0;
+                    timer1.Stop();
+                    MessageBox.Show("Tamamlandi!", "FH", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    backgroundWorker1.CancelAsync();
+                }
+                System.IO.FileInfo fileinfor = new System.IO.FileInfo(openFileDialog1.FileName);
+                filelenghtcontrol = fileinfor.Length;
+                          System.IO.File.AppendAllText(openFileDialog1.FileName, randomcode());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.IO.FileInfo fileinformation = new System.IO.FileInfo(openFileDialog1.FileName);
+            label1.Text = Convert.ToString(fileinformation.Length / 1024 / 1024) + " MB";
+            label2.Text = "Shredd Integer = " + Convert.ToString(shreddedcontrol);
+        }
+    }
+}
